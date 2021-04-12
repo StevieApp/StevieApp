1271db965dfee082cab0f05d8f82554cc1d424fb
Steve Njoroge <njorogesteve31@gmail.com>
2021-04-12 22:19:12 +0300

    alibaba

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,10 +11,34 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void { 
+  ngOnInit(): void {
   }
 
   updateTitle(newTitle: any){
     this.projectTitle = newTitle;
   }
+
+  // onImagesLoaded() {
+  //   var container = document.getElementById("container");
+  //   var images = container?.getElementsByTagName("img");
+  //   var loaded = images?.length;
+  //   for (var i = 0; i < (images as HTMLCollectionOf<HTMLImageElement>).length; i++) {
+  //     console.log(loaded);
+  //     if ((images as HTMLCollectionOf<HTMLImageElement>)[i].complete) {
+  //         (loaded as number)--;
+  //     }else {
+  //       (images as HTMLCollectionOf<HTMLImageElement>)[i].addEventListener("load", ()=> {
+  //         (loaded as number)--;
+  //         if (loaded == 0) {
+  //           console.log('image loaded');
+  //         }
+  //       });
+  //     }
+  //     if (loaded == 0) {
+  //       console.log("All the images have loaded");
+  //     }
+  //   }
+  // }
+
 }
+
