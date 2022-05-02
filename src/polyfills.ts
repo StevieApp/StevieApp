6a372c2492818324cc8d75ab461d0547377759cf
StevieApp <njorogesteve31@gmail.com>
2022-05-02 18:48:12 +0300

    some ad stuff

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -59,7 +59,7 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-
+// adsbygoogle =  window.adsbygoogle || []
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
